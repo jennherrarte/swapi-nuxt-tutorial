01e740eb0243c02d0a0826fbd70e6dc1beeaab74
Jennifer Herrarte <36706323+jennherrarte@users.noreply.github.com>
2022-12-29 20:09:54 -0800

    add axios starships request

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      starships: []
+    }
+  },
+  mounted() {
+    this.getStarships()
+  },
+  methods: {
+    async getStarships() {
+      const res = await this.$axios.$get('/starships')
+      this.starships = res.results
+      console.log(this.starships)
+      return this.starships;
+    }
+  }
 }
+
 </script>
